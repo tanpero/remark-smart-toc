06994efdbff04602570a55969a2864c81fd5065b
tanpero <tanpero@tom.com>
2024-01-26 00:08:03 +0800

    Improve the construction process of the package

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ import { toc } from 'mdast-util-toc'
 import { List } from 'mdast-util-toc/lib'
 
 export default (depth: 1 | 2 | 3 | 4 | 5 | 6) => {
-  return () => (tree, file) => {
+  return () => (tree: any, file: any) => {
 
     // 查找 [toc] 或 [TOC] 标记
     const tocNode = tree.children.find(
-      (node) =>
+      (node: any) =>
         node.type === 'paragraph' &&
         node.children.length === 1 &&
         node.children[0].type === 'text' &&
@@ -15,7 +15,7 @@ export default (depth: 1 | 2 | 3 | 4 | 5 | 6) => {
 
     if (tocNode) {
 
-      const hasHeadings = tree.children.some((node) => /^heading/.test(node.type))
+      const hasHeadings = tree.children.some((node: any) => /^heading/.test(node.type))
 
       const generatedToc = toc(tree, { maxDepth: depth }).map as List
 
